936b5f9d2373d8d91d169bcb25b88d09591a6b40
David Banas <capn.freako@gmail.com>
2017-08-14 05:21:35 -0700

    WARNING: Non-functional commit, for backtracking!

--- switch/src/Language/P4/Interp.hs
+++ switch/src/Language/P4/Interp.hs
@@ -14,8 +14,10 @@
 
 module Language.P4.Interp where
 
+import Control.Lens
 import Control.Monad.State
 import Data.Bits
+import Data.List
 import qualified Data.Map.Strict as Map
 
 -- | Behavioral P4 interpreter.
@@ -50,15 +52,36 @@ data SwitchState = SwitchState
   }
 
 -- | Match/action table abstraction.
+--
+-- TODO: The *Ord* instance needs work. Because table (i.e. - Map)
+--       construction sorts the keys as they're inserted, we can't use
+--       insertion order to indicate priority. And, what really needs
+--       to happen is that exact matches take priority over ternary
+--       matches.
 data Table = Table
   { tableID :: Int
   , rows    :: [TableRow]
   }
+
 data TableRow = TableRow
   { fields  :: Map.Map Field Match
   , params  :: Map.Map Param Value
   , actions :: [Action]
   }
+
+instance Eq TableRow where
+  rx == ry = fields rx == fields ry
+
+instance Ord TableRow where
+  rx <= ry = if xType == yType then x' <= y' else xType <= yType
+    where xType = x Map.! fld
+          yType = y Map.! fld
+          x'    = Map.delete fld x
+          y'    = Map.delete fld y
+          fld   = head $ Map.keys x
+          x     = fields rx
+          y     = fields ry
+
 data Field    = FinPort
               | FoutPort
               | FvlanId
@@ -66,6 +89,7 @@ data Field    = FinPort
               | FdstAddr
               | FeType
   deriving (Show, Eq, Ord)
+
 data Param    = PinPort
               | PoutPort
               | PvlanId
@@ -73,57 +97,81 @@ data Param    = PinPort
               | PdstAddr
               | PeType
   deriving (Show, Eq, Ord)
+
 data Match    = Exact Value
               | Ternary Integer Integer  -- value and mask
               | NoMatch
   deriving (Show, Eq, Ord)
-data Value    = Addr Integer
+
+data Value    = Addr  Integer
               | Etype EthType
+              | VBool Bool
+              | VInt  Int
   deriving (Show, Eq, Ord)
+
+getVal :: Value -> a
+getVal v = case v of
+  Addr  n  -> n
+  Etype et -> et
+  VBool q  -> q
+  VInt  n  -> n
+
 data EthType  = IP
               | NMB
   deriving (Show, Eq, Ord)
+
+-- | Packet abstraction.
+--
+-- TODO: Talk to Conal about my choice of making everything a *Value*.
+--       I did this, in order to avoid introducing existential type
+--       quantification, in my fieldToPktAcc function, below. But, I
+--       need to make sure I'm thinking about this correctly.
+data Pkt = Pkt
+  { -- meta-data
+    inPort  :: Value
+  , outPort :: Value
+  , vlanId  :: Value
+  , dropped :: Value
+    -- header
+  , srcAddr :: Value
+  , dstAddr :: Value
+  , eType   :: Value
+    -- payload
+  , pyldSize :: Value     -- For now, just note the size of the payload.
+  }
+hdrFields = [FsrcAddr, FdstAddr, FeType]
 
 -- | Field matching
 --
 -- TODO: Will Haskell let us map over all possible value constructors for a type?
 --       This manual matching of the function code, below, to the data structures,
 --       above, seems clunky and inelegant.
+{-
 allFieldsMatch :: Map.Map Field Match -> Pkt -> Bool
 allFieldsMatch mp pkt = and $ concat [
   [matchVal (mp Map.! fld) (acc (meta   pkt)) | (fld, acc) <- zip [FinPort,  FoutPort, FvlanId] [inPort,  outPort, vlanId]],
   [matchVal (mp Map.! fld) (acc (header pkt)) | (fld, acc) <- zip [FsrcAddr, FdstAddr, FeType]  [srcAddr, dstAddr, eType]] ]
+-}
+
+rowMatch :: Pkt -> TableRow -> Bool
+rowMatch pkt row = and
+  [matchVal (fields row Map.! fld) (acc pkt) | (fld, acc) <- zip hdrFields $ map fieldToPktAcc hdrFields]
 
 matchVal :: Match -> Value -> Bool
 matchVal mtch v = case mtch of
-  Exact val   -> case val of
-    Addr n | Addr m <- v -> m == n
-           | otherwise   -> False
-    Etype et             -> v == Etype et
-
+  NoMatch   -> True
+  Exact val -> v == val
   Ternary n m | Addr n' <- v -> (n' .&. m) == (n .&. m)
-              | otherwise    -> False
+              | otherwise    -> False  -- error?
 
-  NoMatch     -> True
-
--- | Packet abstraction.
-data Pkt = Pkt
-  { meta    :: Meta
-  , header  :: Header
-  , payload :: Int     -- For now, just note the size of the payload.
-  }
-
-data Meta = Meta
-  { inPort  :: Value
-  , outPort :: Value
-  , vlanId  :: Value
-  }
-
-data Header = Header
-  { srcAddr :: Value
-  , dstAddr :: Value
-  , eType   :: Value
-  }
+fieldToPktAcc :: Field -> Pkt -> Value
+fieldToPktAcc fld = case fld of
+  FinPort  -> inPort
+  FoutPort -> outPort
+  FvlanId  -> vlanId
+  FsrcAddr -> srcAddr
+  FdstAddr -> dstAddr
+  FeType   -> eType
 
 -- | Converts a P4 script into a behavioral model of a programmed switch.
 mkInterp :: P4Script -> P4Interp
@@ -164,25 +212,18 @@ evalExpr _ = True
 -- | Process a single packet, via a single table.
 --
 -- TODO: Incorporate the switch state into this processing.
--- procPkt :: Table -> [Statement] -> [Statement] -> (Pkt, SwitchState) -> (Pkt, SwitchState)
 procPkt :: Table -> [Action] -> [Action] -> Unop (Pkt, SwitchState)
-procPkt tbl hit miss (pkt, st) = let pkt' = applyActions (mActions ++ extras) pkt
+procPkt tbl hit miss (pkt, st) = let pkt' = foldl (.) id (map actionToFunc (mActions ++ extras)) pkt
                                  in (pkt', st)
   where extras | matched == True = hit
                | otherwise      = miss
-        (mActions, matched)      = match tbl pkt
-
-applyActions :: [Action] -> Pkt -> Pkt
-applyActions _ = id
+        (mActions, matched)     = match tbl pkt
 
 -- | Attempt to match a packet, using a single table.
 match :: Table -> Pkt -> ([Action], Bool)
-match tbl pkt | Just row <- safeHead (sortRows matchingRows) = (actions row, True)
-              | otherwise                                    = ([],          False)
-  where matchingRows = filter (\x -> allFieldsMatch (fields x) pkt) $ rows tbl
-
-sortRows :: Unop [TableRow]
-sortRows = id
+match tbl pkt | Just row <- safeHead (sort matchingRows) = (actions row, True)
+              | otherwise                                = ([],          False)
+  where matchingRows = filter (rowMatch pkt) $ rows tbl
 
 -- | P4 scripting abstraction.
 data P4Script = P4Script { ingress :: [Statement]        -- Must exist; P4 programs start here. ("main()" equiv.)
@@ -192,13 +233,18 @@ data P4Script = P4Script { ingress :: [Statement]        -- Must exist; P4 progr
 -- | Control flow abstraction.
 data Statement = Apply Table       [Action]    [Action]  -- optional hit/miss processing
                | If    Expr        Statement   Statement
-               | User  [Statement]                          -- Just allows for modular code writing.
+               | User  [Statement]                       -- Just allows for modular code writing.
 
 -- | Actions
 data Action = Noop
             | Drop
-            | Modify
-            | Compound [Action]
+            | Modify Field Value
+
+actionToFunc :: Action -> Unop Pkt
+actionToFunc a = case a of
+  Noop           -> id
+  Drop           -> set dropped True
+  Modify fld val -> set (fieldToPktAcc fld) (getVal val)
 
 -- | Result
 data MatchResult = Hit
